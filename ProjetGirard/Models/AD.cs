a9453282ef89777a966afde21cc947ad8b54bbf0
fbricchi <lokin84250@live.fr>
2020-01-09 15:40:11 +0100

    Ajout de la base de donnée

---
+++ ProjetGirard/Models/AD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetGirard.Models
+{
+    public class AD
+    {
+        private BDChantierBricchiEntities db;
+
+        public AD()
+        {
+            db = new BDChantierBricchiEntities();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public List<Client> TousLesClients()
+        {
+            return db.Clients.ToList();
+        }
+    }
+}
